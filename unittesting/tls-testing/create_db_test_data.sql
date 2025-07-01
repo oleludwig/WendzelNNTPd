27071f4a95ec589f10897f41f628224369f1e43e
Ole Ludwig <mail@oleludwig.de>
2025-07-02 00:54:13 +0200

    Test mariadb service

--- unittesting/tls-testing/create_db_test_data.sql
+++ unittesting/tls-testing/create_db_test_data.sql
@@ -4,11 +4,11 @@ INSERT INTO newsgroups (id, name, pflag, high) VALUES (3, 'alt.wendzelnntpd.test
 
 INSERT INTO postings (msgid, date, author, newsgroups, subject, lines, header)
 VALUES ('<cdp1@localhost>', 1751106600, 'Testuser <test@example.com>', 'alt.wendzelnntpd.test','Test', 1,
-        'Path: test\nMessage-ID: <cdp1@localhost>\nDate: Sat, 28 Jun 2025 12:30:00 +0200\nLines: 1\nX-WendzelNNTPdBodysize: 9\nnewsgroups: alt.wendzelnntpd.test\nfrom: Testuser <test@example.com>\nsubject: Test\n');
+        'Path: test\r\nMessage-ID: <cdp1@localhost>\r\nDate: Sat, 28 Jun 2025 12:30:00 +0200\r\nLines: 1\r\nX-WendzelNNTPdBodysize: 9\r\nnewsgroups: alt.wendzelnntpd.test\r\nfrom: Testuser <test@example.com>\r\nsubject: Test\r\n');
 
 INSERT INTO postings (msgid, date, author, newsgroups, subject, lines, header)
 VALUES ('<cdp2@localhost>', 1751106601, 'Testuser <test@example.com>', 'alt.wendzelnntpd.test','Test', 1,
-        'Path: test\nMessage-ID: <cdp2@localhost>\nDate: Sat, 28 Jun 2025 12:30:01 +0200\nLines: 1\nX-WendzelNNTPdBodysize: 10\nnewsgroups: alt.wendzelnntpd.test\nfrom: Testuser <test@example.com>\nsubject: Test2\n');
+        'Path: test\r\nMessage-ID: <cdp2@localhost>\r\nDate: Sat, 28 Jun 2025 12:30:01 +0200\r\nLines: 1\r\nX-WendzelNNTPdBodysize: 10\r\nnewsgroups: alt.wendzelnntpd.test\r\nfrom: Testuser <test@example.com>\r\nsubject: Test2\r\n');
 
 INSERT INTO ngposts (msgid, ng, postnum) VALUES ('<cdp1@localhost>', 'alt.wendzelnntpd.test', 1);
 INSERT INTO ngposts (msgid, ng, postnum) VALUES ('<cdp2@localhost>', 'alt.wendzelnntpd.test', 2);
