7371e91742ef2dd81d02e91aa0af2f934f7de50f
Ole Ludwig <mail@oleludwig.de>
2025-07-02 01:05:01 +0200

    Test mariadb service

--- unittesting/tls-testing/create_db_test_data_mysql.sql
+++ unittesting/tls-testing/create_db_test_data_mysql.sql
@@ -3,28 +3,12 @@ INSERT INTO newsgroups (id, name, pflag, high) VALUES (2, 'alt.wendzelnntpd.test
 INSERT INTO newsgroups (id, name, pflag, high) VALUES (3, 'alt.wendzelnntpd.test.post', 'y', 0);
 
 INSERT INTO postings (msgid, date, author, newsgroups, subject, lines, header)
-VALUES ('<cdp1@localhost>', 1751106600, 'Testuser <test@example.com>', 'alt.wendzelnntpd.test',
-        'Test', 1, 'Path: test
-Message-ID: <cdp1@localhost>
-Date: Sat, 28 Jun 2025 12:30:00 +0200
-Lines: 1
-X-WendzelNNTPdBodysize: 9
-newsgroups: alt.wendzelnntpd.test
-from: Testuser <test@example.com>
-subject: Test
-');
+VALUES ('<cdp1@localhost>', 1751106600, 'Testuser <test@example.com>', 'alt.wendzelnntpd.test','Test', 1,
+        'Path: test\r\nMessage-ID: <cdp1@localhost>\r\nDate: Sat, 28 Jun 2025 12:30:00 +0200\r\nLines: 1\r\nX-WendzelNNTPdBodysize: 9\r\nnewsgroups: alt.wendzelnntpd.test\r\nfrom: Testuser <test@example.com>\r\nsubject: Test\r\n');
 
 INSERT INTO postings (msgid, date, author, newsgroups, subject, lines, header)
-VALUES ('<cdp2@localhost>', 1751106601, 'Testuser <test@example.com>', 'alt.wendzelnntpd.test',
-        'Test', 1, 'Path: test
-Message-ID: <cdp2@localhost>
-Date: Sat, 28 Jun 2025 12:30:01 +0200
-Lines: 1
-X-WendzelNNTPdBodysize: 10
-newsgroups: alt.wendzelnntpd.test
-from: Testuser <test@example.com>
-subject: Test2
-');
+VALUES ('<cdp2@localhost>', 1751106601, 'Testuser <test@example.com>', 'alt.wendzelnntpd.test','Test', 1,
+        'Path: test\r\nMessage-ID: <cdp2@localhost>\r\nDate: Sat, 28 Jun 2025 12:30:01 +0200\r\nLines: 1\r\nX-WendzelNNTPdBodysize: 10\r\nnewsgroups: alt.wendzelnntpd.test\r\nfrom: Testuser <test@example.com>\r\nsubject: Test2\r\n');
 
 INSERT INTO ngposts (msgid, ng, postnum) VALUES ('<cdp1@localhost>', 'alt.wendzelnntpd.test', 1);
 INSERT INTO ngposts (msgid, ng, postnum) VALUES ('<cdp2@localhost>', 'alt.wendzelnntpd.test', 2);
